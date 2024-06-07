dcba1a77ddce84e3309e97320330541d0e0a837c
dlllnnz <169079419+dlllnnz@users.noreply.github.com>
2024-06-07 17:08:02 +0600

    наконец-то регистрация есть с хэшем кстати

--- php/add_user.php
+++ php/add_user.php
@@ -15,31 +15,34 @@
     <div class="main">
         <?php 
             // Retrieving form data
-            $userlogin=$_POST['userlogin'];
-            $userpassword=$_POST['userpassword'];
-            $useremail=$_POST['useremail'];
-            $username=$_POST['username'];
-            $usersurname=$_POST['usersurname'];
+            $userlogin = $_POST['userlogin'];
+            $userpassword = $_POST['userpassword']; // Raw user password
+            $useremail = $_POST['useremail'];
+            $username = $_POST['username'];
+            $usersurname = $_POST['usersurname'];
             
+            // Hashing the user password
+            $hashed_password = password_hash($userpassword, PASSWORD_DEFAULT);
+
             // Checking if user login already exists
-            $checkuser = mysqli_query($conn,"SELECT * FROM users WHERE userlogin='$userlogin'");
+            $checkuser = mysqli_query($conn, "SELECT * FROM users WHERE userlogin='$userlogin'");
             $row = mysqli_fetch_array($checkuser);
-            if (!empty($row['userID'])){
+            if (!empty($row['user_id'])){
                 echo "Error. Login already exists";
                 header('refresh:2, url=../login_form.php');
                 exit();
             }
 
-            // Adding new user to the database
-            $adduser = mysqli_query($conn, "INSERT INTO users(userlogin, useremail, userpassword, username, usersurname) VALUES('$userlogin','$useremail','$userpassword','$username','$usersurname')");
-            $checkuser = mysqli_query($conn,"SELECT * FROM users WHERE userlogin='$userlogin'");
+            // Adding new user to the database with the hashed password
+            $adduser = mysqli_query($conn, "INSERT INTO users(userlogin, useremail, userpassword, username, usersurname) VALUES('$userlogin', '$useremail', '$hashed_password', '$username', '$usersurname')");
+            $checkuser = mysqli_query($conn, "SELECT * FROM users WHERE userlogin='$userlogin'");
             $row = mysqli_fetch_array($checkuser);
             if ($adduser){
                 echo "New user added successfully!";
                 // Setting session variables
-                $_SESSION['userID']=$row['userID'];
-                $_SESSION['userlogin']=$row['userlogin'];
-                header('refresh:2, url=..\index.php');
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['userlogin'] = $row['userlogin'];
+                header('refresh:2, url=../index.php');
                 exit();
             }
             else{
