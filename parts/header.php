59ee997413fa485892e884946db4c409dc58a1f1
allbek <135400458+allbek@users.noreply.github.com>
2024-06-07 22:24:05 +0500

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -3,6 +3,16 @@
         <li><a href="choosing_form.php" >организацию</a></li>
         <li><a href="chats_page.php" >чат</a></li>
         <li><a href="php/exit.php">Выход</a></li>
+        <?php 
+        $query = "SELECT organization_id, org_name FROM organizations";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0): ?>
+                <?php while($row = mysqli_fetch_assoc($result)): ?>
+                    <li><a href="choosing_form.php?org_id=<?= $row['organization_id'] ?>"><?= htmlspecialchars($row['org_name']) ?></a></li>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <li>No organizations found</li>
+            <?php endif; ?>
     </ul>
     
     
